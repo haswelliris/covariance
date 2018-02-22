697ac029289c0b7171e79a7acf7a6245aa162818
Haswell <haswelliris@live.com>
2018-02-23 01:01:35 +0800

    内联IsMissingPheno函数

--- src/Covariance.c
+++ src/Covariance.c
@@ -1,31 +1,57 @@
 #include <stdio.h>
 #include <math.h>
 #include <omp.h>
+#include <stdint.h>
+
+#if __FLOAT_WORD_ORDER__ == __ORDER_BIG_ENDIAN__
+
+typedef union
+{
+  double value;
+  struct
+  {
+    u_int32_t msw;
+    u_int32_t lsw;
+  } parts;
+  struct
+  {
+    u_int64_t w;
+  } xparts;
+} ieee_double_shape_type;
+
+#endif
+
+#if __FLOAT_WORD_ORDER__ == __ORDER_LITTLE_ENDIAN__
+
+typedef union
+{
+  double value;
+  struct
+  {
+    u_int32_t lsw;
+    u_int32_t msw;
+  } parts;
+  struct
+  {
+    u_int64_t w;
+  } xparts;
+} ieee_double_shape_type;
+
+#endif
+
+#define EXTRACT_WORDS(ix0,ix1,d)				\
+do {								\
+  ieee_double_shape_type ew_u;					\
+  ew_u.value = (d);						\
+  (ix0) = ew_u.parts.msw;					\
+  (ix1) = ew_u.parts.lsw;					\
+} while (0)
 
 double* MType;
 double* COV;
 int NVAR;
 int NROW;
 int NV;
-
-int IsMissingPheno(double d)
-{
-    //isnan function
-    int32_t hx, lx;
-    EXTRACT_WORDS (hx, lx, d);
-    hx &= 0x7fffffff;
-    hx |= (uint32_t) (lx | (-lx)) >> 31;
-    hx = 0x7ff00000 - hx;
-    if((int) (((uint32_t) hx) >> 31))
-    {
-        return 1;
-    }
-    else if(d - 65.0 <= 1.e-03 && d - 65.0 >= -1.e-03)
-    {
-        return 1;
-    }
-    return 0;
-}
 
 void findcov_(int* _NVAR, int* _NROW, int* _NV, double* _MType, double* _COV)
 {
@@ -56,15 +82,42 @@ void findcov_(int* _NVAR, int* _NROW, int* _NV, double* _MType, double* _COV)
 
             for(iobs = 0; iobs < NROW; iobs++)
             {
-                if(IsMissingPheno(X1[iobs * NVAR]) || IsMissingPheno(X2[iobs * NVAR]))
+
+                //isnan function
+                int32_t hx, lx;
+                double d = X1[iobs * NVAR];
+                EXTRACT_WORDS (hx, lx, d);
+                hx &= 0x7fffffff;
+                hx |= (uint32_t) (lx | (-lx)) >> 31;
+                hx = 0x7ff00000 - hx;
+                if((int) (((uint32_t) hx) >> 31))
                 {
                     NumMissing++;
                 }
-                else
+                else if(d - 65.0 <= 1.e-03 && d - 65.0 >= -1.e-03)
                 {
-                    MeanX1 += X1[iobs * NVAR];
-                    MeanX2 += X2[iobs * NVAR];
-                    Covariance += X1[iobs * NVAR] * X2[iobs * NVAR];
+                    NumMissing++;
+                }
+                else {
+                    d = X2[iobs * NVAR];
+                    EXTRACT_WORDS (hx, lx, X2[iobs * NVAR]);
+                    hx &= 0x7fffffff;
+                    hx |= (uint32_t) (lx | (-lx)) >> 31;
+                    hx = 0x7ff00000 - hx;
+                    if((int) (((uint32_t) hx) >> 31))
+                    {
+                        NumMissing++;
+                    }
+                    else if(d - 65.0 <= 1.e-03 && d - 65.0 >= -1.e-03)
+                    {
+                        NumMissing++;
+                    }else {
+                        MeanX1 += X1[iobs * NVAR];
+                        MeanX2 += X2[iobs * NVAR];
+                        Covariance += X1[iobs * NVAR] * X2[iobs * NVAR];
+
+                    }
+                    
                 }
             }
 
